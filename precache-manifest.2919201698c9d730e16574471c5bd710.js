cf22262a9d3d8971084b37349340f16a25df3372
Alexperkky <alex.perkinson5@gmail.com>
2019-02-13 13:35:27 -0800

    Updates

---
+++ precache-manifest.2919201698c9d730e16574471c5bd710.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "976821e7c7c83f0311bc",
+    "url": "/clickygame/static/js/runtime~main.976821e7.js"
+  },
+  {
+    "revision": "efe92fe03b815735e52d",
+    "url": "/clickygame/static/js/main.efe92fe0.chunk.js"
+  },
+  {
+    "revision": "89a881566e5b9fda7875",
+    "url": "/clickygame/static/js/2.89a88156.chunk.js"
+  },
+  {
+    "revision": "efe92fe03b815735e52d",
+    "url": "/clickygame/static/css/main.351ece51.chunk.css"
+  },
+  {
+    "revision": "f92af30024136a0eec6a6e5c373bd1a4",
+    "url": "/clickygame/index.html"
+  }
+];
